94c4eb52176fb714fa8a412bacf89830426fd6f9
Gavin Li <craftgavin@gmail.com>
2012-07-12 17:04:31 +0800

    ad editor

--- app/application/rest/controllers/AdController.php
+++ app/application/rest/controllers/AdController.php
@@ -9,7 +9,16 @@ class Rest_AdController extends Zend_Rest_Controller
 	
 	public function indexAction()
 	{
-		
+		$sectionId = $this->getRequest()->getHeader('Section_Id');
+		if(is_null($sectionId)) {
+			throw new Exception('section id is null');
+		}
+		$co = App_Factory::_m('Ad');
+		$data = $co->addFilter('sectionId', $sectionId)
+			->addSort('sort', 1)
+			->fetchArr(false);
+			
+		return $this->_helper->json($data);
 	}
 	
 	public function getAction()
@@ -19,7 +28,23 @@ class Rest_AdController extends Zend_Rest_Controller
 	
 	public function postAction()
 	{
+		$sectionId = $this->getRequest()->getHeader('Section_Id');
+		$sectionDoc = App_Factory::_m('Ad_Section')
+			->find($sectionId);
+		if(is_null($sectionDoc)) {
+			throw new Exception('section doc not found with id:'.$sectionId);
+		}
 		
+		$modelString = $this->getRequest()->getParam('model');
+		$jsonArray = Zend_Json::decode($modelString);
+		
+		$doc = App_Factory::_m('Ad')->create($jsonArray);
+		$doc->sectionId = $sectionId;
+		$doc->save();
+		$sectionDoc->updatePreview();
+		
+		$this->getResponse()->setHeader('result', 'sucess');
+		$this->_helper->json(array('id' => $doc->getId()));
 	}
 	
 	public function putAction()
--- app/application/rest/controllers/AdSectionController.php
+++ app/application/rest/controllers/AdSectionController.php
@@ -9,7 +9,9 @@ class Rest_AdSectionController extends Zend_Rest_Controller
 	
 	public function indexAction()
 	{
-		
+		$co = App_Factory::_m('Ad_Section');
+		$data = $co->fetchAll(true);
+        return $this->_helper->json($data);
 	}
 	
 	public function getAction()
@@ -22,11 +24,11 @@ class Rest_AdSectionController extends Zend_Rest_Controller
 		$modelString = $this->getRequest()->getParam('model');
 		$jsonArray = Zend_Json::decode($modelString);
 		
-		$doc = App_Factory::_m('AdSection')->create($jsonArray);
+		$doc = App_Factory::_m('Ad_Section')->create($jsonArray);
 		$doc->save();
 		
 		$this->getResponse()->setHeader('result', 'sucess');
-		$this->_helper->json(array('id' => $attributeDoc->getId()));
+		$this->_helper->json(array('id' => $doc->getId()));
 	}
 	
 	public function putAction()
