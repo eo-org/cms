8343b8050a87c97ba73e229fabfa34dc48232be8
Gavin Li <craftgavin@gmail.com>
2012-08-09 10:54:03 +0800

    added title to book

--- extension/brick/Front/BookpageDetail/BookpageDetail.php
+++ extension/brick/Front/BookpageDetail/BookpageDetail.php
@@ -17,6 +17,17 @@ class Front_BookpageDetail extends Class_Brick_Solid_Abstract
     
     public function configParam($form)
     {
+    	$showTitle = new Zend_Form_Element_Select('param_showTitle', array(
+    		'filters' => array('StringTrim'),
+    		'label' => '显示标题',
+    		'multiOptions' => array('n' => '不显示', 'y' => '显示'),
+    		'required' => false
+    	));
+        $form->addElement($showTitle);
+        
+        $paramArr = array('param_showTitle');
+        $form->setParam($paramArr);
+        
         return $form;
     }
 }
