1f5c4d07f8e1120b334bdeaeccbf50b2ebfd8532
Gavin Li <craftgavin@gmail.com>
2012-08-28 15:12:57 +0800

    git test template copy

--- app/application/admin/controllers/TemplateController.php
+++ app/application/admin/controllers/TemplateController.php
@@ -4,6 +4,186 @@ class Admin_TemplateController extends Zend_Controller_Action
 	public function indexAction()
 	{
 		
+		
+//		$adapter = Zend_Registry::get('mongoAdapter');
+//		
+//		$db = $adapter->getDb();
+//		
+//		$collection = $db->selectCollection("system.users");
+//		
+//		$users = $collection->find();
+//		
+//		Zend_Debug::dump($users);
+//		
+//		echo count($users);
+//		
+//		foreach($users as $user) {
+//			
+//			Zend_Debug::dump($user);
+//			
+////			$user->delete();
+//			echo "ok<br />";
+//		}
+//		
+//		die();
+		
+//		$mongo = Zend_Registry::get('mongo');
+//		$connection = $mongo->selectDb('admin');
+//		
+//		$username = 'templateAdmin';
+//		$password = 'timeToBuildtempLate';
+//		
+//		
+//		$n = $connection->command(array(
+//			'copydbgetnonce' => 1,
+//			'fromhost' => '108.166.114.11'
+//		));
+//		
+//		echo $n['nonce'].'<br />';
+//		
+//		$saltedHash = md5($n['nonce'].$username.md5($username.":mongo:".$password));
+//		
+//		
+//		echo $saltedHash.'<br />';
+//		
+//		
+//		$result = $connection->command(array(
+//			'copydb' => 1,
+//			'fromhost' => '108.166.114.11',
+//			'fromdb' => 'cms_2',
+//			'todb' => 'cms_copy_byphp',
+//			"username" => $username,
+//		    "nonce" => $n["nonce"],
+//		    "key" => $saltedHash
+//		));
+//		
+//		
+//		
+//		Zend_Debug::dump($result);
+//		die();
+	}
+	
+	public function activateAction()
+	{
+		$adapter = Zend_Registry::get('mongoAdapter');
+		
+		$db = $adapter->getDb();
+		
+		$collection = $db->selectCollection("system.users");
+		
+		$users = $collection->find();
+		
+		$hasUser = false;
+		
+		foreach($users as $user) {
+			$hasUser = true;
+			Zend_Debug::dump($user);
+			
+//			$user->delete();
+			echo "ok<br />";
+		}
+		
+		if(!$hasUser) {
+		
+			$username = 'templateAdmin';
+			$password = 'timeToBuildtempLate';
+			$collection->insert(array(
+				'user' => $username,
+				'pwd' => md5($username . ":mongo:" . $password),
+				'readOnly' => true
+			));
+		}
+		die();
+	}
+	
+	public function deactivateAction()
+	{
+		$adapter = Zend_Registry::get('mongoAdapter');
+		
+		$db = $adapter->getDb();
+		
+		$collection = $db->selectCollection("system.users");
+		
+		$collection->remove();
+		die();
+	}
+	
+	public function importAction()
+	{
+		$resourceSiteId = $this->getRequest()->getParam('site-id');
+		
+		$ch = curl_init("http://account.enorange.cn/rest/remote-site/".$resourceSiteId);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		$returnedStr = curl_exec($ch);
+		
+		$returnedObj = Zend_Json::decode($returnedStr, Zend_Json::TYPE_OBJECT);
+		if($returnedObj->result == 'fail') {
+			die($returnedObj->errMsg);
+		}
+		die($returnedObj);
+		$siteFolder = $returnedObj->orgCode;
+		if(isset($returnedObj->siteFolder)) {
+			$siteFolder = $returnedObj->siteFolder;
+		}
+		$orgCode = $returnedObj->orgCode;
+		$destSiteFolder = Class_Server::getSiteFolder();
+		$fromhost = $returnedObj->serverFullName;
+		$fromdb = 'cms_'.$returnedObj->remoteId;
+		
+		
+		
+		
+		$adapter = Zend_Registry::get('mongoAdapter');
+		
+		$targetDbName = $adapter->getDbName();
+		
+		$mongo = Zend_Registry::get('mongo');
+		$connection = $mongo->selectDb('admin');
+		
+		$username = 'templateAdmin';
+		$password = 'timeToBuildtempLate';
+		
+		
+		$n = $connection->command(array(
+			'copydbgetnonce' => 1,
+			'fromhost' => $fromhost
+		));
+		
+		$saltedHash = md5($n['nonce'].$username.md5($username.":mongo:".$password));
+		
+		
+		$result = $connection->command(array(
+			'copydb' => 1,
+			'fromhost' => $fromhost,
+			'fromdb' => $fromdb,
+			'todb' => $targetDbName,
+			"username" => $username,
+		    "nonce" => $n["nonce"],
+		    "key" => $saltedHash
+		));
+		
+		if($destSiteFolder != $siteFolder) {
+			$fileServerMongo = new Mongo('mongodb://craftgavin:whothirstformagic?@58.51.194.8', array('persist' => 'x'));
+			$db = $fileServerMongo->selectDb('service-file');
+			$collection = $db->file;
+			$cursor = $collection->find(array('orgCode' => $siteFolder));
+			
+			$service = Class_Api_Oss_Instance::getInstance();
+			foreach($cursor as $key => $value) {
+				$prefix = $value['orgCode'];
+				$urlname = $value['urlname'];
+				
+				$fromObj = $prefix.'/'.$urlname;
+				$resp = $service->copyObject($fromObj, $destSiteFolder.'/'.$urlname);
+				if($value['isImage']) {
+					$thumbObj = $prefix.'/_thumb/'.$urlname;
+					$resp = $service->copyObject($thumbObj, $destSiteFolder.'/_thumb/'.$urlname);
+				}
+			}
+		}
+		die();
 	}
 	
 	public function useAction()
@@ -23,11 +203,60 @@ class Admin_TemplateController extends Zend_Controller_Action
 //		}
 		
 		
+//		
 		
-		$db = Zend_Registry::get('db');
-		$result = $db->execute('db.copyDatabase("cms_1", "cms_1_cped_php", "108.166.114.11");');
+//		$result = $db->execute('db.copyDatabase("cms_1", "cms_1_cped_php", "108.166.114.11");');
 		
-		Zend_Debug::dump($result);
+//		
+//		$mongo = Zend_Registry::get('mongo');
+//		$connection = $mongo->selectDb('admin');
+//		
+//		
+//		$n = $connection->command(array(
+//			'copydbgetnonce' => 1,
+//			'fromhost' => '108.166.114.11'
+//		));
+//		
+//		Zend_Debug::dump($n);
+//		
+//		$result = $connection->command(array(
+//			'copydb' => 1,
+//			'fromhost' => '108.166.114.11',
+//			'fromdb' => 'cms_1',
+//			'todb' => 'cms_copy_byphp'
+//		));
+//		
+//		
+//		
+//		
+//		Zend_Debug::dump($result);
+		
+		
+//		$m = new Mongo('mongodb://craftgavin:whothirstformagic?@58.51.194.8', array('persist' => 'x'));
+//		
+//		$conn = $m->selectDb('service-file');
+//		
+//		$collection = $conn->file;
+//		
+//		$cursor = $collection->find(array('orgCode' => '502ca9bb6d54614534000000'));
+//		
+//		foreach($cursor as $key => $value) {
+//			$prefix = $value['orgCode'];
+//			$urlname = $value['urlname'];
+//			
+//			$fromObj = $prefix.'/'.$urlname;
+//			
+//			$resp = $service->copyObject($fromObj, 'apple/'.$urlname);
+//			Zend_Debug::dump($resp);
+//			if($value['isImage']) {
+//				$thumbObj = $prefix.'/_thumb/'.$urlname;
+//				
+//				$resp = $service->copyObject($thumbObj, 'apple/_thumb/'.$urlname);
+//				Zend_Debug::dump($resp);
+//			}
+//			
+//			
+//		}
 		
 		die("db copied");
 	}
