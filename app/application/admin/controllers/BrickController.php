290f719386b437afe7a0c94010e2f9858d53d434
Gavin Li <craftgavin@gmail.com>
2012-09-26 21:53:30 +0800

    centralized extension db

--- app/application/admin/controllers/BrickController.php
+++ app/application/admin/controllers/BrickController.php
@@ -33,21 +33,6 @@ class Admin_BrickController extends Zend_Controller_Action
         $this->view->assign('partialHTML', $partialHTML);
     }
     
-//    public function listCreatedAction()
-//    {
-//    	$spriteId = $this->getRequest()->getParam('spriteId');
-//    	
-//    	$tb = Class_Base::_('Brick');
-//    	$selector = $tb->select(false)->setIntegrityCheck(false)
-//	    	->from($tb, array('brickId', 'extName', 'brickName', 'description', 'cssSuffix', 'order'))
-//	    	->order('brick.order')
-//	    	->limitPage(1, 10);
-//	    $rowset = $tb->fetchAll($selector);
-//	    
-//	    $this->view->stageId = $spriteId;
-//	    $this->view->rowset = $rowset;
-//    }
-    
     public function createAction()
     {
     	$layoutId = $this->getRequest()->getParam('layoutId');
@@ -58,9 +43,9 @@ class Admin_BrickController extends Zend_Controller_Action
     		throw new Exception('url error');
     	}
     	
-		$siteDb = Zend_Registry::get('siteDb');
+		$centerDb = Zend_Registry::get('centerDb');
 		$tb = new Zend_Db_Table(array(
-			Zend_Db_Table_Abstract::ADAPTER => $siteDb,
+			Zend_Db_Table_Abstract::ADAPTER => $centerDb,
 			Zend_Db_Table_Abstract::NAME => 'extension_group'
 		));
 		$rowset = $tb->fetchAll();
@@ -84,9 +69,9 @@ class Admin_BrickController extends Zend_Controller_Action
     		throw new Exception('url error');
     	}
 	    
-	    $siteDb = Zend_Registry::get('siteDb');
+	    $centerDb = Zend_Registry::get('centerDb');
     	$tb = new Zend_Db_Table(array(
-    		Zend_Db_Table_Abstract::ADAPTER => $siteDb,
+    		Zend_Db_Table_Abstract::ADAPTER => $centerDb,
     		Zend_Db_Table_Abstract::NAME => 'extension_v1'
     	));
     	$rowset = $tb->fetchAll($tb->select()
